8f1b64c66374d3a6ffb69d7877f6752d67aaa214
MeiChihChang <37042542+MeiChihChang@users.noreply.github.com>
2018-03-11 15:22:11 +0100

    init commit

---
+++ Point.java
@@ -0,0 +1,23 @@
+package com.crypto.entity;
+
+import java.math.BigInteger;
+
+public class Point {
+	
+	BigInteger pointX;
+	BigInteger pointY;
+	
+	public BigInteger getPointX() {
+		return pointX;
+	}
+	public void setPointX(BigInteger pointX) {
+		this.pointX = pointX;
+	}
+	public BigInteger getPointY() {
+		return pointY;
+	}
+	public void setPointY(BigInteger pointY) {
+		this.pointY = pointY;
+	}
+
+}
